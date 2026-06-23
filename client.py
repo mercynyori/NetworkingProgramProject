03564f67c51ed51846cd39da5bd80e097aa2ad18
mercynyori <mmwega13@gmail.com>
2026-06-24 01:31:36 +0200

    Updated

--- client.py
+++ client.py
@@ -1,103 +1,48 @@
 import socket # networking communications
 import threading # sending and receiving at the same time
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 2902))
+
+nickname = input("Choose nicknames:")
 
 
-class ChatClient:
-    """the chat clients can connect to thr server send messages and receive messages and do them simultaneously
-
-    """
-  
-    def __init__(self, host='127.0.0.1', port=2905):
-       # connect to socket the IPv4 and TCP 
-       self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = host
-        self.port = port
-        #storage for nickname, we be there ones user sets it
-        self. nickname = None 
-        self.running = False
-       # we now connect to server and if its not running we raise the expection part
-        
-       try:
-        self.client.connect(("127.0.0.1", 2905))
-        print(f"Connected to server at {host}:{port}")
-       except Exception as error:
-        print(f"Failed to connect : {error}")
-
-
-    def send_message(self): 
-      """here we gat messages and we send to server doesnt block receiving"""
-      #infite while loop to keep accepting user input and msg they send untill they quit
-      while True:
-
-        #lets get their users input first and put in a data structure
-          message = input()
-
-          if  message.lower() == '/exit':
-                print("Disconnecting...")
-                self.client.close()  # Close connection
-          break
-       # send message to server
-          self.client.send(message.encode()) 
-       
-        
-          whole_message = f"{self.nickname}: {message}"
-    
-
-    
-
-    def receive_msg(self): 
-      """ listens  continuosly for an any incoming messages to the user """ 
-      #while loopp 
-      while True:
-        try: 
-          message = self.client.recv(1024).decode()
-
-        
-        #  checking what type of message from the sever
-          if message == "NICK":  
-             
-            #  server will ask for the name
-             self.client.send(self.nickname.encode())
-
-          elif message == "NAME_TAKEN":
-             print("Name has been taken choose another one")
-             self.nickname = input("Your new nickname :")
-             self.client.send(self.nickname.encode())
-
-          
-          elif message == "NAME_EMPTY":
-             print("Your name cannot be empty")
-             self.nickname= input("New nickname: ")
-             self.client.send(self.nickname.encode())
-          
-      
-          else:
-            #  print the normal message of what the other users have
-             print(message)
-
+def receive():
+    while True:
+        try:
+            #waiting for servers message
+            message = client.recv(1024).decode()
+            #check for nick
+            if message == "NICK":
+              #send nickname
+                client.send(nickname.encode()) 
+            else:
+                print(message)
         except:
-         print("User disconnected from the server")
+                print(f"An error has occured")
+                client.close()
+                break
+        
+def send():
+     while True:
+               message = f"{nickname}: {input("")}"
 
-         break
+               #exiting there
+               if  message.lower() == '/exit':
+                print("Disconnecting...")
+                client.close()  # Close connection
+                break
 
-    def run(self):
-       """  here the client stsrts running"""
-       self.nickname = input(("Choose your nickname:"))
-       print(f"{self.nickname} welcome")
-       print("Commands: /create, /join, /rooms, /users")
+
+def run():
+     receive_thread = threading.Thread(target=receive)
+     receive_thread.start()
+     write_thread = threading.Thread(target=run, args=(client,))
+     write_thread.start()
+
+if __name__ == "__main__":
+    run()
+
       
-
-          # the receiving thread the ears listening
-       receive_thread = threading.Thread(target=self.receive_msg) 
-       receive_thread.daemon = True
-       receive_thread.start()
-
-       self.send_message()
-
-      #  the start buttom
-if __name__== "__main__":
-    client = ChatClient()
-    client.run()
 
 
 
--- server.py
+++ server.py
@@ -2,218 +2,87 @@
 import socket #networking communications
 import threading #multiple connections
 
-# chat server class
-class ChatServer:
-     """
-     With this the server can accept multiple connects,store, receive messeges and send broadcasting messeges
-     """
-     def __init__(self, host="127.0.0.1" , port=2905):
 
-#creating the socket for server and client
+host="127.0.0.1"
+port=2902
+
 #the socket(socket.AF_INET uses IPv4 adresses
-#the second argument is all about the Tcp network
+#the second argument is all about the Tcp networ
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-       self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #binding which tells the Os to to listen here
-       self.server.bind(("127.0.0.1", 2905))
-
+server.bind(("127.0.0.1", 2902))
 #waits for connections
-       self.server.listen()
-       print(f"Server is connected to {host}:{port}")
+server.listen()
+print(f"Server is connected to {host}:{port}")
 
-#data structure to store clients socket objets and nicknames
-       self.clients = []
-       self.nicknames = []
 
-# rooms created a dictionary is created when u enter u enter directly
-       self.rooms = {"HOME": []} 
-    # track the rooms that they are in
-       self.user_rooms = {}  
+# stores the socket objects and nickname 
+clients = []
+nicknames = []
 
-       print("=" * 50 )
-       print(f"Server is listening on 27.0.0.1, 2905")
-       print("=" * 50 )
+print(f"Server is listening on 27.0.0.1, 2902")
 
-     def broadcasting(self, message,sender=None):
-      """ here we send messages to every client """
-     #loop through all the clients that we got
-      for client in self.clients:
-           try:
+def broadcast(message):
+    """ The messages goe to everyone"""
+    for client in clients:
+          try:
           #sending message if fails client disconnected
-                client.send(message)
-           except Exception as error:
-               print(f"No able to send to client:{error}")
+             clients.send(message)
+          except Exception as error:
+             print(f"No able to send to client:{error}")
 
-    
-     def broadcast_to_a_specific_room(self, message, room,sender=None):
-       """Broadcasting to a specific roomdepending on what the user used"""
-       if room in self.rooms:
-            for client in self.rooms[room_name]:
-                if client != sender:
-                    try:
-                        client.send(message)
-                    except:
-                        self.remove(client)
-
-     def nickname_getting(self, client):
-          if client in self.clients:
-             return self.nicknames[self.clients.index(client)]
-          return None
-
-     def remove_client(self,client):
-      # here we remove the client once they disconnect
-      """here we clear the client when they disconnect so that we dont have memory leaks"""
-      if client in self.clients:
-        # find the index then use index to get the nickname
-         index = self.clients.index(client)
-         nickname = self.clients.nicknames[index]
-
-# we remove the socket of the index and nickname
-         self.index.remove(index)
-         self.nicknames.remove(nickname)
-
-         client.socket()
-
-# inform everyone that the person has left
-         left_room_message = f"{nickname} has left the room". encode()       
-         self.broadcasting(left_room_message)
-
-         print(f"{nickname} has left the room")
-
-     def handle_client(self, client):
-      """Here the server listens to any message whether broadcasting or single based messeges"""
-      while True:
+def handle_client(client):
+     while True:
           try: # infite loop on and on of receiving thr clients message
-               message = client.recv(1024).decode()
+               message = clients.recv(1024).decode()
                print("Received", message)
-                 # once u get the msg u send to everyone
-               if message: 
-                  self.broadcast(message, sender_client=client)
-               else:
-                  self.remove(client)
 
+          except:
+               if client in clients:
+                  index = clients.index(client) #find there index n nicknames
 
-                   # the user is able to create a chat room
-               if message.startswith("/create"):
-                   parts= message.split(" ")
-                   if len(parts) > 1:
-                         room_name = parts[1]
+                  nickname = nicknames[index]
+                  clients.remove(client) #remove client from list
 
-                              # create room 
-                         if room_name not in self.rooms:
-                               self.rooms[room_name] = []
+                  client.close()# close connection
 
-                              #  remove them from current room
-                         old_room = self.user_rooms.get(client)
-                              
-
-                            
-                         if old_room and client in self.rooms.get(old_room, []):
-                              self.rooms[old_room].remove(client)
-
-                              #  qdd them to the created one
-                         self.rooms[room_name].append(client)
-                         self.user_rooms[client] = room_name
-                         client.send(f"You have joined '{room_name} welcome!".encode())
-
-
-                   else:
-                        client.send("This is a wrong usage of /create <room_name>".encode())
-
-                    # joining a new chatroom
-               if message.startswith("/join"):
-                  parts=message.split(" ")
-                  if len(parts) > 1:
-                     room_name = parts[1]
-
-                       # removing from anyother room
-                     if room_name in self.rooms: # check if the room is in the server
-                          if client in self.user_room: # check if the clinet is in the room
-                              old = self.user_rooms[client] #  safe check 
-                              self.rooms(old).remove(client) #  if they are there we remoeve them
-  
-                              # then now they join their new chat room
-                          self.rooms[room_name].append(client)   #adds the user to the new room
-                          self.user_rooms[client]= room_name  #stores the user in the new room
-                          client.send(f"You have now joined {room_name}".encode())
-
-                            # get the users in the room
-                          users=[] 
-                          for i in self.rooms(room_name):
-                              name = self.get_nickname(c)
-
-                              if name:
-                                 user.append(name)
-                          if users:
-                               client.send(f"Users are : {', '.join(users)}".encode())
-                     else:
-                          client.send(f"Room is nonexistent").encode()
-
-                  else:
-                      client.send(f"This is the wrong usage of : /join <room_name>".encode())
-
-
-               if message.startswith("/rooms"):
-                   client.send(f"Rooms: {', '.join(self.rooms)}".encode())
-
-          except:  # if client has left the room
-            self.remove_client(client)
-            break
-                    
-
-     def accept_client(self):
-      """ Here the server is constantly waiting for someone to come forever, asks for their name  then accepts"""
-      while True:
-         # wait for the client to come
-           client, address = self.server.accept()
-            #  ask for there name 
-           client.send("NICK".encode())
-
-           nickname = None
-
-           while True:
-           
-                 nickname = client.recv(1024).decode().strip() 
-                    #check if we have the name already then tell its gone
-           if nickname in self.nicknames:
-              client.send("NAME_GONE".encode())
-             # ask again 
-              client.send("NICK".encode())
+               broadcast(f" {nickname} has left chat".encode())
+               break
           
-           else:
-               break 
+def receive():
+    while True:
+        #waiting for a connection to come
+        client, address= server.accept()
+        print(f"Connect to server")
+
+          # ask for name
+        client.send("NICK".encode())
+
+        #now you have the name
+        nickname = client.recv(1024).decode()
+
+        #  store the name 
+        nicknames.append(nickname)
+        clients.append(client)
+
+        #   here we tell everyone we have it
+        client.send("Successfully connected".encode())
+        broadcast(f"{nickname} has joined!".encode())
 
 
-                   # add client to the chat and accept them
-           self.clients.append(client)
-           self.nicknames.append(nickname) #  unique names
-           self.broadcast(f"{nickname} welcome!" . encode())
+        thread= threading.Thread(target=handle_client, args=(client,))
+        thread.start()
 
 
-         # added to the default room once accepted
-           self.rooms["HOME"].append(client)
-
-        # always remember that the client is there in the room
-           self.user_rooms[client] = "HOME"
-
-           client.send(f"{nickname} is in Home".encode())
-
-           client.send(f"Commands: /create, /join, /users, /rooms".encode())
-           client.send(f"You have joined Home".encode())
-
-       # create the thread to listen which allows other users to join
-           thread = threading.Thread(target=self.handle_client, args=(client,))
-           thread.start()
-    
-     def run(self):
-        self.accept_client()
-
-    
-    # start button for the server program
 if __name__ == "__main__":
-     server = ChatServer()
-     server.run()
+    receive()  
+
+
+
+
+
      
 
 
