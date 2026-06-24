a76fbe078f32c46903a3cf2158fd2fd87be895f5
mercynyori <mmwega13@gmail.com>
2026-06-24 23:40:32 +0200

    Last commit

--- protocol.py
+++ protocol.py
@@ -1,14 +1,15 @@
 class Room:
 
   def__init__(self, name):
-    self.name = name
-    self.room = {}
+  self.name = name
+  self.room = {}
 
-  def show_users(self):
-    return self.room
-  
   def show_num_users(self):
     return len(self.room)
+  
+  def show_users(self):
+        return self.room
+
   
   def join_room(self,name,sock):
     self.room[name] = sock
@@ -16,20 +17,20 @@ class Room:
   def broadcast(self, message):
     for client in self.room:
 
-self.room[client].send(message.encode('utf-8'))
+        self.room[client].send(message.encode('utf-8'))
 
-def leave_room(self, name, sock)
-if sock == self.room[name]:
-  del self.room[name]
-  else:
-    self.room[name].send("you can't make another player leave the room!".encode('utf-8'))
+  def leave_room(self, name, sock)
+    if sock == self.room[name]:
+      del self.room[name]
+    else:
+      self.room[name].send("you can't make another player leave the room!".encode('utf-8'))
 
 class User:
-  def__init__(self,name,sock,current_room_name,current_room):
-    self.name = name
-    self.sock = sock
-    self.current_room_name = current_room_name
-    self.current_room = current_room
+     def__init__(self ,name ,sock,current_room_name,current_room):
+     self.name = name
+     self.sock = sock
+     self.current_room_name = current_room_name
+     self.current_room = current_room
 
 
 
